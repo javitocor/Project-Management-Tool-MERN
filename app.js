7d1e3cb1e9d3f8833e100023dd085f4b1229fe23
javitocor <javitocor@gmail.com>
2021-09-23 00:13:13 +0700

    add react static files and create a route for  index.html to app.js

--- app.js
+++ app.js
@@ -29,10 +29,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './frontend/build/static')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/', indexRouter);
+app.use('/api/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
