1a86e7347049eb1b709f178e54697326ed9ffb97
javitocor <javitocor@gmail.com>
2021-09-24 10:55:43 +0700

    implement projects actions

---
+++ frontend/src/actions/projects.js
@@ -0,0 +1,39 @@
+import {
+  GET_ALL_PROJECTS, PROJECTS_PENDING, PROJECTS_ERROR,
+  GET_SINGLE_PROJECT, CREATE_PROJECT, UPDATE_PROJECT, DELETE_PROJECT,
+} from '../constants/constants';
+
+
+export const getAllProjects = projectsList => ({
+  type: GET_ALL_PROJECTS,
+  projectsList,
+});
+
+export const getSingleProject = project => ({
+  type: GET_SINGLE_PROJECT,
+  project,
+});
+
+export const createProject = project => ({
+  type: CREATE_PROJECT,
+  project
+});
+
+export const updateProject = project => ({
+  type: UPDATE_PROJECT,
+  project
+});
+
+export const deleteProject = id => ({
+  type: DELETE_PROJECT,
+  id
+});
+
+export const projectsError = error => ({
+  type: PROJECTS_ERROR,
+  error,
+});
+
+export const projectsPending = () => ({
+  type: PROJECTS_PENDING,
+});
